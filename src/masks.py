017a452f4a4e29f3663b343c69beac9988f1f3be
Olga <solevla2016@gmail.com>
2025-12-19 20:48:15 +0300

    Реализована функция, которая принимает номер карты и возвращает ее маску и функция, которая принимает номер счета и возвращает его маску

---
+++ src/masks.py
@@ -0,0 +1,24 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Реализована функция, которая принимает номер карты и возвращает ее маску."""
+    if len(card_number) != 16 or not card_number.isdigit():
+        return None
+
+    masked = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    return masked
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number("card"))
+
+
+def get_mask_account(account_number: str) -> str:
+    """Реализована функция, которая принимает номер счета и возвращает его маску."""
+    if len(account_number) < 4 or not account_number.isdigit():
+        return None
+
+    masked = f"**{account_number[-4:]}"
+    return masked
+
+
+if __name__ == "__main__":
+    print(get_mask_account("account"))
