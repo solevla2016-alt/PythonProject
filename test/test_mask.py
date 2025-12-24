614c2602039a905ba9dec13e37eae666ec768016
Olga <solevla2016@gmail.com>
2025-12-24 22:42:04 +0300

    Добавлены фикстуры, скорректированы тесты

--- test/test_mask.py
+++ test/test_mask.py
@@ -1,22 +1,27 @@
 import pytest
-from src.mask import get_mask_card_number
 
-@pytest.mark.parametrize('card_number, expected', [
-    ('1234567890123456', '1234 56** **** 3456'),
-    ('123456789012345', ""),
-    ('abcde', ""),
-])
+from src.mask import get_mask_account, get_mask_card_number
+
+
+@pytest.mark.parametrize(
+    "card_number, expected",
+    [
+        ("1234567890123456", "1234 56** **** 3456"),
+        ("123456789012345", ""),
+        ("abcde", ""),
+    ],
+)
 def test_get_mask_card_number(card_number, expected):
     assert get_mask_card_number(card_number) == expected
 
 
-from src.mask import get_mask_account
-
-@pytest.mark.parametrize('account_number, expected', [
-        ('1234567890123456', '**3456'),
-        ('123', ""),
-        ('abcdef', ""),
-    ])
+@pytest.mark.parametrize(
+    "account_number, expected",
+    [
+        ("1234567890123456", "**3456"),
+        ("123", ""),
+        ("abcdef", ""),
+    ],
+)
 def test_get_mask_account(account_number, expected):
-        assert get_mask_account(account_number) == expected
-
+    assert get_mask_account(account_number) == expected
--- test/test_processing.py
+++ test/test_processing.py
@@ -1,36 +1,31 @@
 import pytest
+
 from src.processing import filter_by_state, sort_by_date
 
-data = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
 
-@pytest.mark.parametrize('state, expected_length', [
-    ("EXECUTED", 2),
-    ("CANCELED", 2),
-    ("UNKNOWN", 0),
-])
-def test_filter_by_state(state, expected_length):
-    filtered_data = filter_by_state(data, state)
+@pytest.mark.parametrize(
+    "state, expected_length",
+    [
+        ("EXECUTED", 2),
+        ("CANCELED", 1),
+        ("UNKNOWN", 0),
+    ],
+)
+def test_filter_by_state(transactions_data, state, expected_length):
+    filtered_data = filter_by_state(transactions_data, state)
     assert len(filtered_data) == expected_length
 
 
+@pytest.mark.parametrize(
+    "reverse, expected_order",
+    [
+        (True, ["2019-07-03", "2018-10-14", "2018-06-30"]),
+        (False, ["2018-06-30", "2018-10-14", "2019-07-03"]),
+    ],
+)
+def test_sort_by_date(transactions_data, reverse, expected_order):
+    # Передаём transactions_data в функцию sort_by_date
+    sorted_data = sort_by_date(transactions_data, reverse)
 
-
-data = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-]
-
-@pytest.mark.parametrize('reverse, expected_order', [
-    (True, ["2019-07-03", "2018-10-14", "2018-09-12", "2018-06-30"]),
-    (False, ["2018-06-30", "2018-09-12", "2018-10-14", "2019-07-03"]),
-])
-def test_sort_by_date(reverse, expected_order):
-    sorted_data = sort_by_date(data, reverse)
-    actual_dates = [item['date'].split('T')[0] for item in sorted_data]
+    actual_dates = [item["date"].split("T")[0] for item in sorted_data]
     assert actual_dates == expected_order
